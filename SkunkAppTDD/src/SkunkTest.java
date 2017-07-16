65436b1df5bf15553997bb8f1cf3147ea45aedbc
aguru <aguru@10.50.68.193>
2017-07-15 19:42:32 -0600

    Die get value fail

--- SkunkAppTDD/src/SkunkTest.java
+++ SkunkAppTDD/src/SkunkTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 public class SkunkTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPatron() {
+		Die D1 = new Die("A1", "Amar");
+		  int high = 6;
+		  int low = 1;
+		  assertTrue("Error, random is too high", high >= D1.getValue());
+		  assertTrue("Error, random is too low",  low  <= D1.getValue());
+		
 	}
-
 }
