9b5e9422bd4d79ad4ed719ef7411f1a301bd7c4e
aguru <aguru@10.50.68.193>
2017-07-15 19:48:05 -0600

    Test Dice object

--- SkunkAppTDD/src/SkunkTest.java
+++ SkunkAppTDD/src/SkunkTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class SkunkTest {
 
 	@Test
-	public void testPatron() {
+	public void testDie() {
 		Die D1 = new Die();
 		  int high = 6;
 		  int low = 1;
@@ -13,4 +13,17 @@ public class SkunkTest {
 		  assertTrue("Error, random is too low",  low  < D1.roll());
 		
 	}
+	
+	@Test
+	public void testDice() {
+		Dice D1 = new Dice();
+		  int high = 12;
+		  int low = 2;
+		  assertTrue("Error, random is too high", high > D1.roll());
+		  assertTrue("Error, random is too low",  low  < D1.roll());
+		
+	}
+	
+	
+	
 }
